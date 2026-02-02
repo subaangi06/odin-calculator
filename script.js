cf20153d5f3a8beabbfe729ef4eeba9d1487368c
subaangi06 <balaraajsubaangi@gmail.com>
2026-02-02 11:30:16 +0530

    Write basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,23 @@
+let add = function(num1, num2){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return num1+num2;
+}
+
+let subtract = function(num1, num2){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return num1-num2;
+}
+
+let multiply = function(num1,num2){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return num1*num2;
+}
+
+let divide = function(num1, num2){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    return num1/num2;
+}
