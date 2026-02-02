6b80d010c5516f2affd58376b5df8215af680007
subaangi06 <balaraajsubaangi@gmail.com>
2026-02-02 13:25:28 +0530

    Add basic styling, no javascript logic implemented yet

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ let divide = function(num1, num2){
     num2 = parseInt(num2);
     return num1/num2;
 }
+
+//initialising variables needed for each operation
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+let operate = function(operator, num1, num2){
+    switch (operator){
+        case "+":
+            add(num1, num2);
+        case "-":
+            subtract(num1, num2);
+        case "*":
+            multiply(num1,num2);
+        case "/":
+            divide(num1,num2);
+    }
+}
