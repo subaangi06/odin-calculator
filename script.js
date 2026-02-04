db1ec388b643e15d053b547701c8713a70a87bf5
subaangi06 <balaraajsubaangi@gmail.com>
2026-02-04 13:02:40 +0530

    Add simple arithmetic functionality
    Need to make following functions: delete, ac, decimal, percentage

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
-let add = function(num1, num2){
+const numbers = document.querySelectorAll(".number");
+const operatorInputs = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal")
+const display = document.querySelector("#display"); 
+const decimal = document.querySelector("#decimal");
+const clear = document.querySelector("#ac");
+const backspace = document.querySelector("#del");
+
+const add = function(num1, num2){
     num1 = parseInt(num1);
     num2 = parseInt(num2);
     return num1+num2;
 }
 
-let subtract = function(num1, num2){
+const subtract = function(num1, num2){
     num1 = parseInt(num1);
     num2 = parseInt(num2);
     return num1-num2;
 }
 
-let multiply = function(num1,num2){
+const multiply = function(num1,num2){
     num1 = parseInt(num1);
     num2 = parseInt(num2);
     return num1*num2;
 }
 
-let divide = function(num1, num2){
+const divide = function(num1, num2){
     num1 = parseInt(num1);
     num2 = parseInt(num2);
     return num1/num2;
 }
 
-//initialising variables needed for each operation
-let num1 = 0;
-let num2 = 0;
-let operator = "";
-
-let operate = function(operator, num1, num2){
+const operate = function(operator, num1, num2){
     switch (operator){
         case "+":
-            add(num1, num2);
+            return add(num1, num2);
         case "-":
-            subtract(num1, num2);
+            return subtract(num1, num2);
         case "*":
-            multiply(num1,num2);
+            return multiply(num1,num2);
         case "/":
-            divide(num1,num2);
+            return divide(num1,num2);
     }
 }
+
+//initialising variables needed for each operation
+let calculatedVal = "";
+let newVal = "";
+let operator = "";
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        value = number.textContent;
+        newVal +=value;
+        display.textContent=value;
+    })
+});
+
+operatorInputs.forEach((operatorIn) => {
+    operatorIn.addEventListener("click", () => {
+        operator = operatorIn.textContent;
+        if (operator =="×"){
+            operator = "*";
+        }
+        calculatedVal = newVal;
+        newVal = "";
+    })
+});
+
+
+
+equal.addEventListener("click", () => {
+    display.textContent += "=";
+    console.log(operator);
+    console.log(calculatedVal);
+    console.log(newVal);
+    let result= operate(operator, calculatedVal, newVal);
+    newVal = result;
+    display.textContent=newVal;
+})
