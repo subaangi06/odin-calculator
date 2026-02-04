4574fa07eb378f34a6879d10641b1b989a3ee134
subaangi06 <balaraajsubaangi@gmail.com>
2026-02-04 15:16:34 +0530

    Added delete, ac, decimal. Need to add percentage and keyboard functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//----------------Select essential keys--------------------
 const numbers = document.querySelectorAll(".number");
 const operatorInputs = document.querySelectorAll(".operator");
 const equal = document.querySelector("#equal")
@@ -5,28 +6,30 @@ const display = document.querySelector("#display");
 const decimal = document.querySelector("#decimal");
 const clear = document.querySelector("#ac");
 const backspace = document.querySelector("#del");
+const buttons = document.querySelectorAll(".btn");
 
+//----------------------arithmetic operations------------------
 const add = function(num1, num2){
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1+num2;
 }
 
 const subtract = function(num1, num2){
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1-num2;
 }
 
 const multiply = function(num1,num2){
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1*num2;
 }
 
 const divide = function(num1, num2){
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1/num2;
 }
 
@@ -43,16 +46,17 @@ const operate = function(operator, num1, num2){
     }
 }
 
-//initialising variables needed for each operation
-let calculatedVal = "";
-let newVal = "";
+//--------------functionalities----------------
+
+let calculatedVal = ""; //value entered right before operator
+let newVal = ""; //value currently being entered
 let operator = "";
 
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
         value = number.textContent;
         newVal +=value;
-        display.textContent=value;
+        display.textContent=newVal;
     })
 });
 
@@ -61,20 +65,69 @@ operatorInputs.forEach((operatorIn) => {
         operator = operatorIn.textContent;
         if (operator =="×"){
             operator = "*";
+        } else if (operator =="÷"){
+            operator = "/";
         }
         calculatedVal = newVal;
-        newVal = "";
+        newVal = ""; //input is cleared after operator entered, so if equal is pressed right after operator, we get NaN
     })
 });
 
 
 
 equal.addEventListener("click", () => {
-    display.textContent += "=";
     console.log(operator);
     console.log(calculatedVal);
     console.log(newVal);
+    if (newVal === "" || operator ==="") return; //do nothing if operator or number is not input after operator but equal button is pressed
     let result= operate(operator, calculatedVal, newVal);
-    newVal = result;
-    display.textContent=newVal;
+    if (result === Infinity){
+        display.textContent = "did you just divide by 0";
+        newVal = "";
+    } else {
+        if(result.toString().split("").length > 13){
+            result = result.toString().split("").slice(0,13).join("");
+        }
+        newVal = result;
+        display.textContent=newVal;
+    }
+
 })
+
+clear.addEventListener("click", () => {
+    display.textContent = "0";
+    calculatedVal = "";
+    newVal = "";
+    operator = "";
+
+})
+
+backspace.addEventListener("click", () => {
+    displayContent = display.textContent;
+    displayContent = displayContent.split("").slice(0,-1).join("");
+    newVal = displayContent;
+    display.textContent = displayContent;
+})
+
+buttons.forEach((button) => {
+    button.addEventListener("mouseenter", () => {
+        button.style.backgroundColor = "#4d4b4b";
+    })
+    button.addEventListener("mouseleave", () => {
+        button.style.backgroundColor = "#242222";
+    })
+})
+
+decimal.addEventListener("click", () => {
+    if (display.textContent.includes(".")){
+        return;
+    }
+    if (display.textContent ==="" || display.textContent ==="0"){
+        newVal = "0.";
+    } else{
+        newVal +="."
+    }
+    display.textContent = newVal;
+
+})
+
